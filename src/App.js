f17e5dd39bee3130114fd336f721f46edcabad7a
Z3key <galaxy.s5.n950a@gmail.com>
2025-11-08 16:23:19 +0200

    Finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  // const count = 0;
+  const [count, setCount] = useState(0);
+
+  const add = () => setCount(count + 1);
+  const minus = () => setCount(count - 1);
+  const reset = () => setCount(0);
+
+  if (count < 0) {
+    alert("Negative numbers aren't supported");
+    reset();
+  }
+
   return (
+    <div style={styles.container}>
+      <h1>Click Counter</h1>
+      <p>Count: {count}</p>
+
     <div>
-      Hello, React!
+      <button onClick={add}>Add</button>
+      <button onClick={minus}>Minus</button>
+      <button onClick={reset}>Reset</button>
     </div>
+    </div>
+
   );
+}
+
+const styles = {
+  container : {
+    textAlign: 'center',
+  },
+
+  heading : {
+
+  }
 }
 
 export default App;
